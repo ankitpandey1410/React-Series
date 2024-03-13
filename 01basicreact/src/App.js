f24e908dfbbdac72afe040321207d742974dda0a
Ankit Kumar <imakki1410@gmail.com>
2024-03-13 19:56:34 +0530

    Basic Conventional Rule

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,12 @@
-
+import Chai from "./Chai";
 
 function App() {
   return (
-    <h1>React Series</h1>
+    <>
+      <h1>React Series</h1>
+      <Chai/>
+    </>
+    
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,15 @@
-
+import Chai from "./chai"
 
 function App() {
 
 
   return (
-    <h1>React Series with Vite</h1>
+    <>
+      <h1>React Series with Vite</h1>
+      <Chai/>
+      <p>Test para</p>
+    </>
+    
   )
 }
 
